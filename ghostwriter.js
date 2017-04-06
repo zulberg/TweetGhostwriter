6100d458afc749908f36ab3e77b02e0b50cc8b1b
Unknown <zulberg@mail.csuchico.edu>
2017-04-06 12:11:34 -0700

    x21 lab completed

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,15 @@
 function writeTweet()
 {
-  
+var intro=["There's always", "No escaping", "I can't stand", "Stay away from"];
+var body=["stupid people,", "fast food joints,", "credit card offers,", "reality shows,", "love songs,"];
+var end=["they will wreck your life.", "they will never die."];
+var randIndex=randomUpTo(intro.length-1);
+var sentence=intro[randIndex];
+randIndex=randomUpTo(body.length-1);
+sentence=sentence+" "+body[randIndex];
+randIndex=randomUpTo(end.length-1);
+sentence=sentence+" "+end[randIndex];
+document.getElementById("tweet").innerHTML=sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
